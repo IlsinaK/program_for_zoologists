e303120a16c16d0c71e1478694f2736a14b056a5
Ilsina Komarova <ilsinchik@list.ru>
2024-11-27 17:32:05 +0300

    add AnimalParamTest

--- src/test/java/AnimalParamTest.java
+++ src/test/java/AnimalParamTest.java
@@ -13,38 +13,26 @@ public class AnimalParamTest {
 
     private final String animalKind;
     private final List<String> expectedFood;
-    private final boolean shouldThrowException;
 
-    public AnimalParamTest(String animalKind, List<String> expectedFood, boolean shouldThrowException) {
+
+    public AnimalParamTest(String animalKind, List<String> expectedFood) {
         this.animalKind = animalKind;
         this.expectedFood = expectedFood;
-        this.shouldThrowException = shouldThrowException;
+
     }
 
     @Parameterized.Parameters
     public static Object[][] data() {
         return new Object[][]{
-                {"Травоядное", List.of("Трава", "Различные растения"), false},
-                {"Хищник", List.of("Животные", "Птицы", "Рыба"), false},
-                {"Некорректный вид", null, true}  // Проверка некорректного ввода
+                {"Травоядное", List.of("Трава", "Различные растения")},
+                {"Хищник", List.of("Животные", "Птицы", "Рыба")},
         };
     }
 
     @Test
     public void getFoodTest() throws Exception {
         Animal animal = new Animal();
-
-        if (shouldThrowException) {
-            try {
-                animal.getFood(animalKind);
-            } catch (Exception e) {
-                assertEquals("Неизвестный вид животного, используйте значение Травоядное или Хищник", e.getMessage());
-                return; // Успешно поймано исключение
-            }
-            throw new AssertionError("Ожидалось исключение, но его не было");
-        } else {
-            List<String> food = animal.getFood(animalKind);
-            assertEquals(expectedFood, food);
-        }
+        List<String> food = animal.getFood(animalKind);
+        assertEquals(expectedFood, food);
     }
 }
