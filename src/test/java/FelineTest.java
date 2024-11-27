58e6ba3caa465ba8bdc3080db2af2aebb72c49c0
Ilsina Komarova <ilsinchik@list.ru>
2024-11-27 19:35:00 +0300

    add FelineTest class

--- src/test/java/FelineTest.java
+++ src/test/java/FelineTest.java
@@ -1,4 +1,5 @@
 import com.example.Feline;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
@@ -6,29 +7,32 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 public class FelineTest {
+    private Feline feline;
+
+    @Before
+    public void setUp() {
+        feline = new Feline();
+    }
 
     @Test
     public void getFamilyTest() {
-        Feline feline = new Feline();
+
         assertEquals("Кошачьи", feline.getFamily());
     }
 
     @Test
     public void getKittensTest() {
-        Feline feline = new Feline();
         assertEquals(1, feline.getKittens());
+    }
+
+    @Test
+    public void getKittensWithParameterTest() {
         assertEquals(3, feline.getKittens(3));
     }
-    @Test
-    public void testGetKittensWithParameter() {
-        Feline feline = new Feline();
-        assertEquals(3, feline.getKittens(3)); // Проверяем метод с параметром
-    }
+
 
     @Test
     public void testEatMeat() throws Exception {
-        Feline feline = new Feline();
         assertEquals(List.of("Животные", "Птицы", "Рыба"), feline.eatMeat());
     }
-
 }
