9fa32b5b657cc88c8d8b41c92dd47ecd699b1e30
Ilsina Komarova <ilsinchik@list.ru>
2024-11-27 20:02:22 +0300

    add LionMockTest class

--- src/test/java/LionMockTest.java
+++ src/test/java/LionMockTest.java
@@ -1,5 +1,6 @@
 import com.example.Feline;
 import com.example.Lion;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -9,21 +10,26 @@ import static org.junit.Assert.assertEquals;
 
 public class LionMockTest {
 
+ private Feline feline;
+ private Lion lion;
+
+ @Before
+ public void setUp() throws Exception {
+     feline = Mockito.mock(Feline.class);
+     lion = new Lion(feline, "Самец");
+ }
+
     @Test
     public void getKittensTest() throws Exception {
-        Feline feline = Mockito.mock(Feline.class);
-        Mockito.when(feline.getKittens()).thenReturn(3);
+       Mockito.when(feline.getKittens()).thenReturn(3);
 
-        Lion lion = new Lion(feline, "Самец");
         assertEquals(3, lion.getKittens());
     }
 
     @Test
     public void getFoodTest() throws Exception {
-        Feline feline = Mockito.mock(Feline.class);
         Mockito.when(feline.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
 
-        Lion lion = new Lion(feline, "Самец");
         List<String> food = lion.getFood();
         assertEquals(List.of("Животные", "Птицы", "Рыба"), food);
     }
